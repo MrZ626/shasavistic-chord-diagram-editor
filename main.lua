b63fcb05cd45b7401e25bf46ae4c8c10951c78b2
MrZ_26 <1046101471@qq.com>
2025-07-14 01:49:06 +0800

    修删除最后一个和弦崩溃

--- main.lua
+++ main.lua
@@ -303,8 +303,11 @@ function editor:deleteChord(s, e)
     for i = e, s, -1 do
         rem(self.chordList, i)
     end
+    if #self.chordList == 0 then
+        self.cursor = 0
+        self:newChord()
+    end
     if self.cursor > #self.chordList then self:moveCursor(0) end
-    if #self.chordList == 0 then self:newChord() end
 end
 
 function editor:switchTheme()
