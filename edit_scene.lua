a382a7fcc73482f81dd8a981fa5db582612db757
MrZ_26 <1046101471@qq.com>
2025-08-19 21:13:23 +0800

    按任意键将不再显示隐藏的光标

--- edit_scene.lua
+++ edit_scene.lua
@@ -37,7 +37,6 @@ end
 
 function scene.keyDown(key, isRep)
     if key == 'lctrl' or key == 'rctrl' or key == 'lshift' or key == 'rshift' or key == 'lalt' or key == 'ralt' then return true end
-    if not toggles.cursor then toggles.cursor = true end
     local CTRL = KBisDown('lctrl', 'rctrl')
     local SHIFT = KBisDown('lshift', 'rshift')
     local ALT = KBisDown('lalt', 'ralt')
