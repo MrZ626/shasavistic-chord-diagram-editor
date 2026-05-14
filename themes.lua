44cce7395e2ace7810bc68d4d1e56a62962bc25f
MrZ_26 <1046101471@qq.com>
2026-05-14 17:32:01 +0800

    亮色主题会自动填充部分数据

--- themes.lua
+++ themes.lua
@@ -98,19 +98,7 @@ local themes = {
             { COLOR.HEX '59C0B5FF' }, -- 9
             { COLOR.HEX 'DB66B2FF' }, -- 10
             { COLOR.HEX 'DA7FDBFF' }, -- 11
-            { COLOR.HEX '90F196FF' }, -- 12 unofficial from here
-            { COLOR.HEX 'ABCC5AFF' }, -- 13
-            { COLOR.HEX 'D4CC57FF' }, -- 14
-            { COLOR.HEX 'F09E65FF' }, -- 15
-            { COLOR.HEX 'E075DAFF' }, -- 16
-            { COLOR.HEX '9192F6FF' }, -- 17
-            { COLOR.HEX '84B1F8FF' }, -- 18
-            { COLOR.HEX '8FF3D7FF' }, -- 19
-            { COLOR.HEX '91F7BAFF' }, -- 20
-            { COLOR.HEX '91F39EFF' }, -- 21
-            { COLOR.HEX '92D973FF' }, -- 22
-            { COLOR.HEX 'A9CD5CFF' }, -- 23
-            -- Add more dimensions here
+            -- Add more dimensions here (optional, will be auto-filled with faded colors if not provided)
         },
         dimGridColor = {
             { COLOR.HEX 'AAAAAA62' }, -- 1
@@ -124,19 +112,7 @@ local themes = {
             { COLOR.HEX '59C0B562' }, -- 9
             { COLOR.HEX 'DB66B262' }, -- 10
             { COLOR.HEX 'DA7FDB62' }, -- 11
-            { COLOR.HEX '90F19626' }, -- 12 unofficial from here
-            { COLOR.HEX 'ABCC5A26' }, -- 13
-            { COLOR.HEX 'D4CC5726' }, -- 14
-            { COLOR.HEX 'F09E6526' }, -- 15
-            { COLOR.HEX 'E075DA26' }, -- 16
-            { COLOR.HEX '9192F626' }, -- 17
-            { COLOR.HEX '84B1F826' }, -- 18
-            { COLOR.HEX '8FF3D726' }, -- 19
-            { COLOR.HEX '91F7BA26' }, -- 20
-            { COLOR.HEX '91F39E26' }, -- 21
-            { COLOR.HEX '92D97326' }, -- 22
-            { COLOR.HEX 'A9CD5C26' }, -- 23
-            -- Add more dimensions here
+            -- Add more dimensions here (optional, will be auto-filled with faded colors if not provided)
         },
     },
 }
@@ -148,6 +124,10 @@ local function fadeColor(back, fore, a)
         fore[3] * a + back[3] * (1 - a)
     }
 end
+
+TABLE.update(themes.bright.dim, themes.dark.dim)
+TABLE.update(themes.bright.dimGridColor, themes.dark.dimGridColor)
+
 themes.dark.dimFade = {}
 for i = 1, #themes.dark.dim do themes.dark.dimFade[i] = fadeColor(themes.dark.bg, themes.dark.dim[i], .42) end
 themes.bright.dimFade = {}
